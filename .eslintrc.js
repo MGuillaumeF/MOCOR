61bcc7a10625a42fe3ac22e74f76e0505f7b6e00
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2021-03-17 18:57:26 +0100

    Ajout du fichier de configuration pour eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": [
+    // react hooks recommended rules configuration 
+    "plugin:react-hooks/recommended"
+  ],
+  "plugins": [
+    "react-hooks"
+  ],
+  "rules": {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "error"
+  }
+};
