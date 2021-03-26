cba34fe9b1bb1b087dcb352ce78e7fd5c4ac163b
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2021-03-27 00:54:34 +0100

    Mise à jour des configuration

---
+++ styleguide.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+    propsParser: require('react-docgen-typescript').withCustomConfig(
+      './tsconfig.json'
+    ).parse,
+    exampleMode: 'expand',
+    usageMode: 'expand',
+    styleguideDir: 'docs',
+    pagePerSection: true,
+    ignore: [
+      '**/index.tsx',
+      '**/__tests__/**',
+      '**/*.test.{js,jsx,ts,tsx}',
+      '**/*.spec.{js,jsx,ts,tsx}',
+      '**/*.d.ts'
+    ],
+    theme: {
+      color: {
+        baseBackground: '#333',
+        base: '#FFF',
+        sidebarBackground: '#303030',
+        codeProperty: '#ff008c',
+        codeDeleted: '#ff008c'
+      }
+    },
+    sections: [
+      {
+        name: 'Introduction',
+        content: 'README.md'
+      },
+      {
+        name: 'Documentation',
+        pagePerSection: true,
+        description: 'The description for the documentation section',
+        sections: [
+          {
+            name: 'Components',
+            pagePerSection: true,
+            components: 'src/components/**/*.tsx',
+            exampleMode: 'expand', // 'hide' | 'collapse' | 'expand'
+            usageMode: 'expand' // 'hide' | 'collapse' | 'expand'
+          }
+        ]
+      },
+      {
+        name: 'Live Demo',
+        external: true,
+        href: 'https://github.com/MGuillaumeF/MOCOR#readme'
+      }
+    ]
+  };
