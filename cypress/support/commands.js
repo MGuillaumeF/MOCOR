a283b959b12501b46977d4834140de0ffe23e7f0
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2021-03-27 01:25:56 +0100

    Correction pre-commit

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,7 +24,7 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-const COMMAND_DELAY = 2000;
+const COMMAND_DELAY = 300;
 
 for (const command of [
   'visit',
